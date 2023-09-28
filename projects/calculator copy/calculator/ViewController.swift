8ca40e2d7619ce40592912e62adf0dfabf973350
Paul Stewart <paul.stewart.professional@gmail.com>
2023-09-28 10:11:34 -0600

    calc pt. 2 finished

---
+++ projects/calculator copy/calculator/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Paul Stewart on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    @IBOutlet weak var posNegButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var divisionButton: UIButton!
+    
+    @IBOutlet weak var multiplicationButton: UIButton!
+    
+    @IBOutlet weak var additionButton: UIButton!
+    
+    @IBOutlet weak var subtractionButton: UIButton!
+    
+    @IBOutlet weak var equalsButton: UIButton!
+    
+    @IBOutlet weak var button9: UIButton!
+    
+    @IBOutlet weak var button8: UIButton!
+    
+    @IBOutlet weak var button7: UIButton!
+    
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBOutlet weak var button5: UIButton!
+    
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button0: UIButton!
+    
+    @IBOutlet weak var decimalPoint: UIButton!
+    
+    @IBOutlet weak var numbersLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clearButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func posNegButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func divisionButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func multiplicationButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func additionButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func subtractionButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func equalsButtonPressed(_ sender: Any) {
+    }
+    
+}
+
