31d4ccb744185bdc94d3c42e38ac7103389a11eb
Paul Stewart <paul.stewart.professional@gmail.com>
2023-10-25 09:56:55 -0600

    added more sections to table view. yuhhh

--- projects/hotelReservations/hotelReservations/AddRegistrationTableViewController.swift
+++ projects/hotelReservations/hotelReservations/AddRegistrationTableViewController.swift
@@ -9,6 +9,60 @@ import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
     
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    let checkInDateLabelCellIndexPath = IndexPath(row: 0, section: 1)
+    let checkOutDateLabelCellIndexPath = IndexPath(row: 2, section: 1)
+
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where
+            isCheckInDatePickerVisible == false:
+            return 0
+        case checkOutDatePickerCellIndexPath where
+            isCheckOutDatePickerVisible == false:
+            return 0
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+//    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//    }
+    
+    override func tableView(_ tableView: UITableView,
+       didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == checkInDateLabelCellIndexPath &&
+           isCheckOutDatePickerVisible == false {
+            // check-in label selected, check-out picker is not visible, toggle check-in picker
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelCellIndexPath &&
+           isCheckInDatePickerVisible == false {
+            // check-out label selected, check-in picker is not visible, toggle check-out picker
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelCellIndexPath ||
+           indexPath == checkOutDateLabelCellIndexPath {
+            // either label was selected, previous conditions failed meaning at least one picker is visible, toggle both
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -16,32 +70,69 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var checkOutDateLabel: UILabel!
     @IBOutlet weak var checkInDatePicker: UIDatePicker!
     @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    @IBOutlet var numberOfAdultsLabel: UILabel!
+    @IBOutlet var numberOfAdultsStepper: UIStepper!
+    @IBOutlet var numberOfChildrenLabel: UILabel!
+    @IBOutlet var numberOfChildrenStepper: UIStepper!
+    
+    @IBAction func wifiSwitch(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let midnightToday = Calendar.current.startOfDay(for: .now)
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        updateNumberOfGuests()
     }
-
+    
     // MARK: - Table view data source
     
     @IBAction func doneBarButtonItemTapped(_ sender: Any) {
-            let firstName = firstNameTextField.text ?? ""
-            let lastName = lastNameTextField.text ?? ""
-            let email = emailTextField.text ?? ""
-            print("DONE TAPPED")
-            print("firstName: \(firstName)")
-            print("lastName: \(lastName)")
-            print("email: \(email)")
-        }
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+//        let checkInDate = checkInDatePicker.date
+//        let checkOutDate = checkOutDatePicker.date
+//        let adultCount = Int(numberOfAdultsStepper.value)
+//        let childCount = Int(numberOfChildrenStepper.value)
         
+        print("DONE TAPPED")
+        print("firstName: \(firstName)")
+        print("lastName: \(lastName)")
+        print("email: \(email)")
     }
     
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        checkOutDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+    }
     
+    func updateNumberOfGuests() {
+        numberOfAdultsLabel.text =
+           "\(Int(numberOfAdultsStepper.value))"
+        numberOfChildrenLabel.text =
+           "\(Int(numberOfChildrenStepper.value))"
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+        updateNumberOfGuests()
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        updateDateViews()
+    }
+    
+    @IBAction func wifiSwitchValueChanged(_ sender: Any) {
+    }
+    
+}
+
+
+
+
 //
 //    override func numberOfSections(in tableView: UITableView) -> Int {
 //        // #warning Incomplete implementation, return the number of sections
@@ -79,7 +170,7 @@ class AddRegistrationTableViewController: UITableViewController {
 //            tableView.deleteRows(at: [indexPath], with: .fade)
 //        } else if editingStyle == .insert {
 //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }    
+//        }
 //    }
 //    */
 //
