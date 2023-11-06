b130df824a560762dafd8df404933f48cba019aa
Paul Stewart <paul.stewart.professional@gmail.com>
2023-11-06 14:25:32 -0700

    started list and got bored so started family project

--- Student Unit 2/1 - Tables and Persistence/3 - Model View Controller/lab/FavoriteAthlete/FavoriteAthlete/AthleteTableViewController.swift
+++ Student Unit 2/1 - Tables and Persistence/3 - Model View Controller/lab/FavoriteAthlete/FavoriteAthlete/AthleteTableViewController.swift
@@ -15,7 +15,32 @@ class AthleteTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    @IBSegueAction func addAthlete(_ coder: NSCoder, sender: Any?) -> AthleteFormViewController? {
+        let athleteFormVC = AthleteFormViewController(coder: coder, athlete: nil)
+        return athleteFormVC
+    }
+    
+    @IBSegueAction func editAthlete(_ coder: NSCoder, sender: Any?) -> AthleteFormViewController? {
+        let athleteToEdit: Athlete?
+        if let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            athleteToEdit = athletes[indexPath.row]
+        } else {
+            athleteToEdit = nil
+        }
+        return AthleteFormViewController(coder: coder, athlete: athleteToEdit)
+    }
+    
+    @IBAction func unwindToAthleteTableVCFromAthleteForm(_ segue: UIStoryboardSegue) {
+        guard let athleteFormViewController = segue.source as? AthleteFormViewController,
+              let athlete = athleteFormViewController.athlete else { return }
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            athletes[selectedIndexPath.row] = athlete
+        } else {
+            athletes.append(athlete)
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return athletes.count
     }
