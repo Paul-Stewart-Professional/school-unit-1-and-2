99abf7b22c43b7f2b9bbeebce5b2c5b974a90ea7
Paul Stewart <paul.stewart.professional@gmail.com>
2023-11-14 16:33:59 -0700

    ToDo list up to part 7

--- projects/toDoList/ToDo.swift
+++ projects/toDoList/ToDo.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct ToDo: Equatable {
+    let id = UUID()
+    let title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "Todo one", isComplete: false, dueDate: Date(), notes: "notes one")
+        let todo2 = ToDo(title: "Todo two", isComplete: false, dueDate: Date(), notes: "notes two")
+        let todo3 = ToDo(title: "Todo three", isComplete: false, dueDate: Date(), notes: "notes three")
+        
+        return [todo1, todo2, todo3]
+    }
+    
+    static let dueDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
